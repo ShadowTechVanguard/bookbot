3d4c65a21c032189fe7c448c716a8591873cf1c8
ShadowTechVanguard <ShadowTechVanguard@protonmail.com>
2025-04-23 12:34:59 -0500

    Final update to allow searching of any book

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import sys
 def main():
-    from stats import get_num_words
-    print(get_num_words())
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    from stats import get_num_words, char_frequency, read_book, sorted_char_list
+    txt = sys.argv[1]
+    read = read_book(txt)
+    char = char_frequency(read)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {txt}...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(read)} total words")
+    print("--------- Character Count -------")
+    for item in sorted_char_list(char):
+        print(f"{item['Char']}: {item['Num']}")
+        if item == len(sorted_char_list(char)):
+            break
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,9 +1,28 @@
-def get_num_words(): 
-    filepath = 'books/frankenstein.txt'
-    word_count = 0
-    with open(filepath) as f:
+def read_book(txt):
+    with open(txt) as f:
         file_contents = f.read()
-        #print(file_contents)
-        for words in file_contents.split():
-            word_count += 1
-        return (f"{word_count} words found in the document")
+    return file_contents
+
+def sort_on(dict):
+    return dict["Num"]
+
+def get_num_words(read): 
+    words = read.split()
+    return len(words)
+    
+def char_frequency(read):
+    character_dic = {} 
+    for char in read.lower():
+        if char.isalpha():
+            if char in character_dic:
+                character_dic[char] += 1
+            else:
+                character_dic[char] = 1
+    return character_dic
+
+def sorted_char_list(char):
+    sorted_char = []
+    for ch in char:
+        sorted_char.append({"Char": ch, "Num":char[ch]})
+    sorted_char.sort(reverse=True, key=sort_on)
+    return sorted_char
